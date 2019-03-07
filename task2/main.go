cb7ca033121a1f03d213fb253c23fd71feaca7bd
Biazhkou <biazhkou@yandex.ru>
2019-03-08 00:26:24 +0300

    task2

--- task2/main.go
+++ task2/main.go
@@ -1,9 +1,23 @@
 package main
 
-// Specify MapTo function here
+func MapTo(inputData []int, fn func(item, i int) string) []string {
+	var result []string
+	for index, value := range inputData {
+		result = append(result, fn(value, index))
+	}
+	return result
+}
 
 func Convert(arr []int) []string {
-	// converting nimber code should be here
+	fn := func(item, i int) string {
+		numbers := map[int]string{1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
+		value, ok := numbers[item]
+		if ok {
+			return value
+		}
+		return "unknown"
+	}
+	return MapTo(arr, fn)
 }
 
 func main() {
